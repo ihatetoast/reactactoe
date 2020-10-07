d9a9b275375e55df252cca642787cfe6ebdf51f3
Katy <ihatetoast@gmail.com>
2020-10-06 19:36:28 -0500

    Updates

---
+++ precache-manifest.3d8dc57694cddb3d8c527ddc707b8cb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420c0cb856e9c3ea25e857a2b08aed2e",
+    "url": "/ihatetoast/reactactoe/index.html"
+  },
+  {
+    "revision": "2f91c2ba3dc38750f594",
+    "url": "/ihatetoast/reactactoe/static/css/main.ce12ac2b.chunk.css"
+  },
+  {
+    "revision": "82de1f8cd1c4c82796cf",
+    "url": "/ihatetoast/reactactoe/static/js/2.6d7c0404.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ihatetoast/reactactoe/static/js/2.6d7c0404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f91c2ba3dc38750f594",
+    "url": "/ihatetoast/reactactoe/static/js/main.fc043ce2.chunk.js"
+  },
+  {
+    "revision": "026f6108d027aca3ad5a",
+    "url": "/ihatetoast/reactactoe/static/js/runtime-main.f0e2ade8.js"
+  },
+  {
+    "revision": "a7446f2c611d3c7625968539d3de5cdc",
+    "url": "/ihatetoast/reactactoe/static/media/egg.a7446f2c.png"
+  },
+  {
+    "revision": "71050117670f1944e6df7025006f5313",
+    "url": "/ihatetoast/reactactoe/static/media/friedegg.71050117.png"
+  },
+  {
+    "revision": "af9e876e6fd7836f4ed788d9ae32041f",
+    "url": "/ihatetoast/reactactoe/static/media/gameboard-a.af9e876e.svg"
+  },
+  {
+    "revision": "f602537f9c667df15d669bb84dce592f",
+    "url": "/ihatetoast/reactactoe/static/media/gameboard-i.f602537f.svg"
+  },
+  {
+    "revision": "0ca1de6b982dbcb21dadb6fe221b571a",
+    "url": "/ihatetoast/reactactoe/static/media/gameboard-o.0ca1de6b.svg"
+  },
+  {
+    "revision": "3c5ce6a6f306be7ea57bd54c1d694dc8",
+    "url": "/ihatetoast/reactactoe/static/media/tictactoeBoard_lg.3c5ce6a6.svg"
+  }
+]);
